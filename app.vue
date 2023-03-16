b8b8c1104c519a3729fd6621b99babb96e652d3b
stonesilver12 <stonesilver12@gmail.com>
2023-03-16 03:03:05 +0100

    Update: ♻️code update

--- app.vue
+++ app.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="bg-slate-900 flex-1 flex flex-col justify-center items-center">
-    <Welcome/>
+    <Welcome @next="nextStep" v-if="step === 0"/>
+    <InspectionEngine @next="nextStep" v-if="step === 1"/>
   </div>
 </template>
 <script setup lang="ts">
 import Welcome from "~/components/Welcome.vue";
+import InspectionEngine from "~/components/InspectionEngine.vue";
+
+const step = ref<number>(0)
+
+const nextStep: () => void = () => {
+  step.value = step.value + 1
+}
 </script>
---
+++ components/InspectionEngine.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex-1 w-full relative">
+    <!-- Snap Btn -->
+    <div class="absolute flex justify-center bottom-16 w-full">
+      <div class="w-[65px] h-[65px] rounded-full bg-white flex justify-center items-center">
+        <div class="w-[60px] h-[60px] rounded-full bg-white border border-slate-500"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useInspection} from "~/store/inspection";
+
+defineComponent({
+  name: 'InspectionEngine',
+})
+
+const inspection = useInspection()
+</script>
+
+<style scoped>
+
+</style>
